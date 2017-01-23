5b8cfa76ecc8975893a89dceea48a211fc8a97c5
Nick Sterling <nicholas.v.sterling@gmail.com>
2017-01-23 10:22:33 -0700

    Update build.sh

--- elastic-data/nyc_collision_builder/build.sh
+++ elastic-data/nyc_collision_builder/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sleep 20
-tar xvzf nyc_collision_data.tar.gz
+
+echo "Waiting for services to start..."
+sleep 15
+echo "Uncompressing data source..."
+#tar xvzf nyc_collision_data.tar.gz
+xz -d -k nyc_collision_data.csv.xz
+echo "Uncpmpress complete! Importing data now..."
 nc logstash 5000 < ./nyc_collision_data.csv
+echo "Data import complete";
 rm nyc_collision_data.csv
